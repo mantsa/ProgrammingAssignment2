53b59d3bc2270ddfd012e68b65982dda2c95cee7
mantsa <DE-70918@49070918-002.de.ema.ad.pwcinternal.com>
2014-05-23 15:01:27 +0200

    week3 submission

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,66 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(a=matrix()){
+    
+#######################################################
+###This function creates a matrix with the special 
+###ability to cache their inverse. Following operations 
+###are allowed: 
+###
+### a) m<-makeCacheMatrix() - creates an empty matrix
+### b) m$set(y) - set the values of the matrix
+### c) setinv() - calculates the inverse matrix, if it 
+###     wasn't already calculated
+### d) getinv() - returns the cached inverse matrix
+#######################################################
+    
+    a_i<-NULL   #inititialize the inverse matrix as null
+    #setter for the source matrix
+    set<-function(b){
+        #check first if the given matrix already exists
+        if(!identical(a,b)){
+            a<<-b
+            a_i<<-NULL
+        }
+    }
+    
+    #getter for the source matrix 
+    get <- function() a
+    
+    #setter for the inverse matrix
+    setinv <- function(solve=matrix()) a_i<<-solve
+    #getter of the inverse matrix
+    getinv <- function() a_i
+    #define the setter & getter option to the object
+    list(set=set, get=get, setinv=setinv, getinv=getinv)
 }
 
+cacheSolve <- function(a,...){
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#######################################################
+###This function calculates the inverse matrix of a 
+###given object from type 'makeCacheMatrix'. To create 
+###such type of matrix please use the constructor
+###function makeCacheMatrix().
+###
+##########################################################    
+    
+    ##Get the inverse matrix of the given one
+    i<-a$getinv()
+    #if the inverse matrix is already calculated (not equals null) get it from the cache
+    if(!is.null(i)){ 
+        message("Getting cached data")
+        return(i)        
+    }else{
+        data<-a$get()
+        #make a check if the matrix is 
+        if(dim(data)[1]==dim(data)[2]){
+            message("Calculating the inverse matrix")
+            i<-solve(data,...)
+            a$setinv(i)
+            
+        }else {
+            print("For the given matrix it could not be calculated an inverse one.") 
+            print("Please try with a squared matrix.")
+        }
+        return(i)
+    }
 }
